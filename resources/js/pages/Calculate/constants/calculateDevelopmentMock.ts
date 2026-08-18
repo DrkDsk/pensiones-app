cd4727c1f6c8b058d1a6dc0c2ffcc1e4586f54f9
Alfredo Palacios <joseaph.1998@gmail.com>
2026-08-18 11:24:05 -0600

    refactor: remove unused `modalidad40AnnualValues` and add default `modalidad10` value in `calculateDevelopmentMock`

--- resources/js/pages/Calculate/constants/calculateDevelopmentMock.ts
+++ resources/js/pages/Calculate/constants/calculateDevelopmentMock.ts
@@ -93,7 +93,6 @@ export const calculateDevelopmentMock = {
         modalidad10CostPercentage: '',
         modalidad40CostPercentage: '',
         modalidad40UmaMultiplier: 25,
-        modalidad40AnnualValues: {},
         pagoRetroactivo: 205167,
         pagoAyudaDeDesempleo: 93860.61,
         seguroDeVida: 12759.35,
@@ -106,6 +105,7 @@ export const calculateDevelopmentMock = {
             contribution_end_date: '',
             contribution_start_date: '',
         },
+        modalidad10: '',
     },
     projection: {
         monthlyPayment: 0,
